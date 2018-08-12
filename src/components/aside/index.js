8c0dc78bfa1154ccc06e45a6f19366ae377073a5
Dylan Alcazar <dylan.alcazar@gmail.com>
2018-08-12 13:54:48 -0600

    wip

--- src/components/aside/index.js
+++ src/components/aside/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import Countries from './countries/';
 
 const Aside = () => {
-    return (
-        <div className='cont'>
-            <div className='searched-countries'>
-                <Countries />
-            </div>
-        </div>
-    );
+  return (
+    <div className='cont'>
+      <div className='searched-countries'>
+        <Countries />
+      </div>
+    </div>
+  );
 }
 
 export default Aside;
--- src/components/countryDetail/index.js
+++ src/components/countryDetail/index.js
@@ -2,45 +2,52 @@ import React from 'react';
 
 class countryDetail extends React.Component {
 
-    constructor(props) { //setea initial state and component first time
-        super(props);
-        this.state = {
-            country: [], //data from api
-            isLoaded: false,// isLoaded: false, //when items are loaded
-        }
+  constructor(props) { //setea initial state and component first time
+    super(props);
+    this.state = {
+      country: [], //data from api
+      isLoaded: false,// isLoaded: false, //when items are loaded
     }
+  }
 
-    componentDidMount() {
-        
-        this.setState({
-            country: JSON.parse(this.props.location.query.obj),
-            isLoaded: true
-        })
+  componentDidMount() {
+
+    setTimeout(() => {
+      this.setState({
+        country: JSON.parse(this.props.location.query.obj),
+        isLoaded: true
+      })
+    }, 1000);
+  }
+
+  render() {
+
+    var { isLoaded, country } = this.state;
+
+
+    if (!isLoaded) {
+      return (
+        <div className="d-flex justify-content-center align-items-center">
+          <div className="circle left rotate"><span></span></div>
+          <div className="circle right rotate"><span></span></div>
+        </div>
+      );
     }
+    else {
+      return (
 
-    render() {
-
-        var { isLoaded, country } = this.state;
-
-
-        if (!isLoaded) {
-            return <h1>Loading...</h1>
-        }
-        else {
-            return (
-
-                <div className='container'>
-                    <h1>{country.name}</h1>
-                    <h3>Capital: {country.capital}</h3>
-                    <h3>Region: {country.region}</h3>
-                    <h3>Subregion: {country.subregion}</h3>
-                    <h3>Native Name: {country.nativeName}</h3>
-                    <h3>Area: {country.area} km²</h3>
-                    <h3>Demonym: {country.demonym}</h3>
-                </div>
-            )
-        }
+        <div className='container'>
+          <h1>{country.name}</h1>
+          <h3>Capital: {country.capital}</h3>
+          <h3>Region: {country.region}</h3>
+          <h3>Subregion: {country.subregion}</h3>
+          <h3>Native Name: {country.nativeName}</h3>
+          <h3>Area: {country.area} km²</h3>
+          <h3>Demonym: {country.demonym}</h3>
+        </div>
+      )
     }
+  }
 }
 
 export default countryDetail;
