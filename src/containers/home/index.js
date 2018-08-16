0c5f0f5727480d7494f5be0101665c79495835fe
Dylan Alcazar <dylan.alcazar@gmail.com>
2018-08-16 10:21:30 -0600

    using json server

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -4,63 +4,85 @@ import 'babel-polyfill';
 import Aside from '../../components/aside/';
 import Main from '../../components/main/';
 
-const api = 'https://restcountries.eu/rest/v2';
+//const api = 'https://restcountries.eu/rest/v2';
+const api = 'http://localhost:3000/countries';
 
 class Home extends React.Component {
 
   constructor(props) { //set initial state and component first time
     super(props);
     this.state = {
-      items: [], //will => data from api
+      countries: [],
       isLoaded: false,
+      error: null
     }
   }
 
   async componentDidMount() {
 
-    const res = await fetch(api);
-    const items = await res.json();
+    try {
+      const response = await fetch(api)
+      const data = await response.json()
 
-    await setTimeout(() => {
       this.setState({
-        items,
+        countries: data,
         isLoaded: true
       })
-    }, 1000);
-
-
+    } catch (error) {
+      this.setState({
+        error,
+      })
+    }
   }
 
   render() {
 
-    var { isLoaded, items } = this.state;
-
-    if (!isLoaded) {
-      return (
-        <div className="d-flex justify-content-center align-items-center">
-          <div className="lds-css ng-scope">
-            <div className="lds-gear"><div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>
+    var { isLoaded, countries, error } = this.state;
+    return (
+      isLoaded ? <div className='row row--my row--mycont'>
+        <div className=' offset-1 col-md-2 col-sm-2'>
+          <aside>
+            <Aside />
+          </aside>
+        </div>
+        <div className='offset-1 col-md-7 col-sm-7'>
+          <div className="container">
+            <Main countries={countries} />
           </div>
         </div>
-      );
-    }
+      </div>
+        : error ? <p>Error...</p>
+          : <div className="d-flex justify-content-center">
+            <div className="lds-css ng-scope">
+              <div className="lds-gear"><div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div></div>
+          </div>
+    )
 
-    else {
-      return (
-        <div className='row row--my row--mycont'>
-          <div className=' offset-1 col-md-2 col-sm-2'>
-            <aside>
-              <Aside />
-            </aside>
-          </div>
-          <div className='offset-1 col-md-7 col-sm-7'>
-            <div className="container">
-              <Main countries={items} />
-            </div>
-          </div>
-        </div>
-      );
-    }
+    /* if (!isLoaded) {
+       return (
+         <div className="d-flex justify-content-center align-items-center">
+           <div className="circle left rotate"><span></span></div>
+           <div className="circle right rotate"><span></span></div>
+         </div>
+       );
+     }
+ 
+     else {
+       return (
+         <div className='row row--my row--mycont'>
+           <div className=' offset-1 col-md-2 col-sm-2'>
+             <aside>
+               <Aside />
+             </aside>
+           </div>
+           <div className='offset-1 col-md-7 col-sm-7'>
+             <div className="container">
+               <Main countries={items} />
+             </div>
+           </div>
+         </div>
+       );
+     }*/
   }
 }
 
