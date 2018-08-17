6dc2b1bfac848217744e75b5c7fc2f17fe314fe2
Dylan Alcazar <dylan.alcazar@gmail.com>
2018-08-17 09:36:08 -0600

    wip wwComponents

--- src/components/countryDetail/detailMenu/index.js
+++ src/components/countryDetail/detailMenu/index.js
@@ -2,20 +2,22 @@ import React from "react";
 import { Switch, Route, Link } from 'react-router-dom';
 
 
-const DetailMenu = ({country}) => {
+const DetailMenu = ({ country }) => {
   return (
-    <ul className="nav nav-pills mb-3" id="myTab" role="tablist">
-      <li className="nav-item">
-        <Link to='/' className="nav-link active">
-          Country
+    <div className='container'>
+      <ul className="nav nav-pills mb-3" id="myTab" role="tablist">
+        <li className="nav-item">
+          <Link to='/' className="nav-link active">
+            Country
               </Link>
-      </li>
-      <li className="nav-item">
-        <Link to={{ pathname: `/provinces/${country.name}`, state: country.provinces }} className="nav-link">
-          Provinces
+        </li>
+        <li className="nav-item">
+          <Link to={{ pathname: `/provinces/${country.name}`, state: country.provinces }} className="nav-link">
+            Provinces
               </Link>
-      </li>
-    </ul>
+        </li>
+      </ul>
+    </div>
   )
 }
 
